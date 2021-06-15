c93c37ff895a3ea44c8344402f68e1748ae685ab
SaltyKitkat <954537646@qq.com>
2021-06-15 20:55:41 +0800

    pass all tests

--- src/lib.rs
+++ src/lib.rs
@@ -1,19 +1,21 @@
-pub struct KvStore;
+use std::collections::HashMap;
+
+pub struct KvStore(HashMap<String, String>);
 
 impl KvStore {
     pub fn new() -> Self {
-        unimplemented!()
+        KvStore(HashMap::new())
     }
 
     pub fn set(&mut self, key: String, value: String) -> Option<String> {
-        unimplemented!()
+        self.0.insert(key, value)
     }
 
     pub fn get(&mut self, key: String) -> Option<String> {
-        unimplemented!()
+        self.0.get(&key).and_then(|s| Some(s.to_owned()))
     }
 
     pub fn remove(&mut self, key: String) -> Option<String> {
-        unimplemented!()
+        self.0.remove(&key)
     }
 }
