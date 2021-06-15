cb6abf7c7e01b21fdc6cccf8f57a0e4f00fd200b
SaltyKitkat <954537646@qq.com>
2021-06-15 20:37:31 +0800

    cli part finished

--- src/bin/kvs.rs
+++ src/bin/kvs.rs
@@ -1,3 +1,34 @@
-fn main(){
-    println!("Hello world!!!");
+use std::process::exit;
+
+use clap::{App, Arg, SubCommand};
+
+fn main() {
+    let matches = App::new(env!("CARGO_PKG_NAME"))
+        .version(env!("CARGO_PKG_VERSION"))
+        .subcommands(vec![
+            SubCommand::with_name("set")
+                .arg(Arg::with_name("key").takes_value(true).required(true))
+                .arg(Arg::with_name("value").takes_value(true).required(true)),
+            SubCommand::with_name("get")
+                .arg(Arg::with_name("key").takes_value(true).required(true)),
+            SubCommand::with_name("remove")
+                .alias("rm")
+                .arg(Arg::with_name("key").takes_value(true).required(true)),
+        ])
+        .get_matches();
+    match matches.subcommand() {
+        ("set", Some(subm)) => {
+            eprintln!("unimplemented");
+            exit(255)
+        }
+        ("get", Some(subm)) => {
+            eprintln!("unimplemented");
+            exit(255)
+        }
+        ("remove", Some(subm)) => {
+            eprintln!("unimplemented");
+            exit(255)
+        }
+        _ => exit(1),
+    }
 }
