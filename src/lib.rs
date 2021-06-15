058cd89ef3f99a8a1b3f04b09489739e02ba36ee
SaltyKitkat <954537646@qq.com>
2021-06-15 19:54:18 +0800

    update deps and fix compile for test

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,19 @@
-fn main() {
-    println!("Hello, world!");
+pub struct KvStore;
+
+impl KvStore {
+    pub fn new() -> Self {
+        unimplemented!()
+    }
+
+    pub fn set(&mut self, key: String, value: String) -> Option<String> {
+        unimplemented!()
+    }
+
+    pub fn get(&mut self, key: String) -> Option<String> {
+        unimplemented!()
+    }
+
+    pub fn remove(&mut self, key: String) -> Option<String> {
+        unimplemented!()
+    }
 }
