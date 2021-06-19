a9e3d1c17efdbb4e343f6e37e772da2a02163937
SaltyKitkat <954537646@qq.com>
2021-06-19 12:06:00 +0800

    impl compaction

--- src/kv.rs
+++ src/kv.rs
@@ -3,15 +3,15 @@
 use crate::error::{Result, *};
 use crate::seekablefile::SeekableLSFile;
 use serde::{Deserialize, Serialize};
+use std::fs::rename;
 use std::io::SeekFrom;
-use std::{
-    collections::HashMap,
-    path::PathBuf,
-};
+use std::{collections::HashMap, path::PathBuf};
 /// there is just a warpper for a HashMap.
 pub struct KvStore {
     index: HashMap<String, u64>,
+    path: PathBuf,
     disk_db: SeekableLSFile,
+    invalid_count: u64,
 }
 #[derive(Serialize, Deserialize, Debug)]
 enum Log {
@@ -25,8 +25,9 @@ impl KvStore {
     pub fn open(path: impl Into<PathBuf>) -> Result<Self> {
         let mut path = path.into();
         path.push("mydb");
-        let mut disk_db = SeekableLSFile::new(path)?;
+        let mut disk_db = SeekableLSFile::new(&path)?;
         let mut index = HashMap::new();
+        let mut invalid_count = 0;
         let mut offset = 0;
         loop {
             let mut buf = String::new();
@@ -35,33 +36,45 @@ impl KvStore {
                 break;
             }
             match ron::de::from_str(&buf)? {
-                Log::Set(k, _) => index.insert(k, offset),
-                Log::Rm(k) => index.remove(&k),
+                Log::Set(k, _) => {
+                    if index.insert(k, offset).is_some() {
+                        invalid_count += 1;
+                    }
+                }
+                Log::Rm(k) => {
+                    index.remove(&k);
+                    invalid_count += 1;
+                }
             };
             offset += l as u64;
         }
-        Ok(Self { index, disk_db })
+        Ok(Self {
+            index,
+            disk_db,
+            path,
+            invalid_count,
+        })
     }
     /// Set the value of a string key to a string.
-    /// Return an error if the value is not written successfully.    
+    /// Return an error if the value is not written successfully.
     pub fn set(&mut self, key: String, value: String) -> Result<()> {
         let set_log = Log::Set(key.to_owned(), value.to_owned());
         let mut buf = ron::ser::to_string(&set_log)?;
         buf.push('\n');
         let (offset, _) = self.disk_db.append(buf.as_bytes())?;
-        self.index.insert(key, offset);
+        if self.index.insert(key, offset).is_some() {
+            self.invalid_count += 1;
+            self.compaction_trigger()?;
+        };
         Ok(())
     }
+
     /// Get the string value of a string key.
     /// If the key does not exist, return `None`.
     /// Return an error if the value is not read successfully.
     pub fn get(&mut self, key: String) -> Result<Option<String>> {
         if let Some(&pos) = self.index.get(&key) {
-            // self.disk_db.seek(SeekFrom::Start(pos))?;
-            let mut buf = String::new();
-            self.disk_db
-                .read_line_from(SeekFrom::Start(pos), &mut buf)?;
-            match ron::from_str(&buf)? {
+            match inner_get(&mut self.disk_db, pos)? {
                 Log::Rm(_) => Err(KvsError::Inner.into()),
                 Log::Set(k, v) => {
                     if k.eq(&key) {
@@ -83,9 +96,51 @@ impl KvStore {
             rm_log.push('\n');
             self.disk_db.append(rm_log.as_bytes())?;
             self.index.remove(&key);
+            self.invalid_count += 1;
+            self.compaction_trigger()?;
             Ok(())
         } else {
             Err(KvsError::KeyNotFound(key).into())
         }
     }
+
+    fn compaction_trigger(&mut self) -> Result<()> {
+        if self.invalid_count >= self.index.len() as u64 {
+            self.compaction()
+        } else {
+            Ok(())
+        }
+    }
+
+    fn compaction(&mut self) -> Result<()> {
+        let mut file_name = self
+            .path
+            .file_name()
+            .map_or("mydb".into(), |s| s.to_string_lossy())
+            .to_string();
+        file_name.push_str(".new");
+        let mut new_path = self.path.clone();
+        new_path.set_file_name(file_name);
+
+        let mut new_db = SeekableLSFile::new(&new_path)?;
+        let Self { index, disk_db, .. } = self;
+        for (_, pos) in index {
+            let log = inner_get(disk_db, *pos)?;
+            let mut buf = ron::ser::to_string(&log)?;
+            buf.push('\n');
+            let (tmp, _) = new_db.append(buf.as_bytes())?;
+            *pos = tmp;
+        }
+        rename(&new_path, &self.path)?;
+        self.path = new_path;
+        self.disk_db = new_db;
+        self.invalid_count = 0;
+        Ok(())
+    }
+}
+
+fn inner_get(disk_db: &mut SeekableLSFile, pos: u64) -> Result<Log> {
+    let mut buf = String::new();
+    disk_db.read_line_from(SeekFrom::Start(pos), &mut buf)?;
+    Ok(ron::from_str(&buf)?)
 }
--- src/seekablefile.rs
+++ src/seekablefile.rs
@@ -1,17 +1,17 @@
+#![deny(missing_docs)]
 use crate::error::Result;
 use std::{
     fs::{File, OpenOptions},
     io::{BufRead, BufReader, Seek, SeekFrom, Write},
     path::Path,
 };
-
 pub(crate) struct SeekableLSFile {
     inner: File,
 }
 
-// todo: impl sth for this and come up with ideas to impl the read method.
 impl SeekableLSFile {
-    pub fn new(p: impl AsRef<Path>) -> Result<Self> {
+    /// give me a path and I will open a file for you.
+    pub(crate) fn new(p: impl AsRef<Path>) -> Result<Self> {
         let inner = OpenOptions::new()
             .create(true)
             .read(true)
@@ -19,21 +19,16 @@ impl SeekableLSFile {
             .open(p)?;
         Ok(Self { inner })
     }
-
-    pub fn read_line_from(&mut self, offset: SeekFrom, buf: &mut String) -> Result<usize> {
+    /// read a line from offset, store them into buf and return the lenth
+    pub(crate) fn read_line_from(&mut self, offset: SeekFrom, buf: &mut String) -> Result<usize> {
         self.inner.seek(offset)?;
         let len = BufReader::new(&mut self.inner).read_line(buf)?;
         Ok(len)
     }
 
-    pub fn append(&mut self, b: &[u8]) -> Result<(u64, usize)> {
+    pub(crate) fn append(&mut self, b: &[u8]) -> Result<(u64, usize)> {
         let s = self.inner.seek(SeekFrom::End(0))?;
         let len = self.inner.write(b)?;
         Ok((s, len))
     }
 }
-
-// impl Seek for SeekableLSFile {
-//     fn seek(&mut self, pos: SeekFrom) -> std::io::Result<u64> {
-//     }
-// }
