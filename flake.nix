d2ae2a99a68898103dbc1506097020690db1d4a4
SaltyKitkat <954537646@qq.com>
2022-03-20 22:06:38 +0800

    use nix

---
+++ flake.nix
@@ -0,0 +1,36 @@
+{
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+    rust-overlay = {
+      url = "github:oxalica/rust-overlay";
+      inputs.nixpkgs.follows = "nixpkgs";
+      inputs.flake-utils.follows = "flake-utils";
+    };
+  };
+  outputs = { self, flake-utils, nixpkgs, ... } @ inputs:
+    flake-utils.lib.eachSystem [ "x86_64-linux" ] (system:
+      let pkgs = import nixpkgs
+        {
+          inherit system;
+          config.allowUnfree = true;
+          overlays = [
+            inputs.rust-overlay.overlay
+          ];
+        }; in
+      {
+        devShell = with pkgs; mkShell {
+          buildInputs = [
+            nixpkgs-fmt
+
+            clang_13
+            lld_13
+            lldb_13
+            rust-analyzer
+            rust-bin.stable.latest.default
+          ];
+        };
+      }
+    )
+  ;
+}
