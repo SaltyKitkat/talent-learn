459680e2e8a81794858339a61bd75a7a9bff6183
SaltyKitkat <954537646@qq.com>
2022-03-20 23:41:16 +0800

    not use bufio in client

--- src/client.rs
+++ src/client.rs
@@ -4,28 +4,21 @@ use crate::{
 };
 use serde::Deserialize;
 use serde_json::Deserializer;
-use std::{
-    io::{BufReader, BufWriter, Write},
-    net::{TcpStream, ToSocketAddrs},
-};
+use std::net::{TcpStream, ToSocketAddrs};
 
 pub struct KvsClient {
-    reader: BufReader<TcpStream>,
-    writer: BufWriter<TcpStream>,
+    tcp_stream: TcpStream,
 }
 
 impl KvsClient {
     pub fn connect(socket: impl ToSocketAddrs) -> Result<Self> {
-        let stream = TcpStream::connect(socket)?;
-        let reader = BufReader::new(stream.try_clone()?);
-        let writer = BufWriter::new(stream);
-        Ok(Self { reader, writer })
+        let tcp_stream = TcpStream::connect(socket)?;
+        Ok(Self { tcp_stream })
     }
     pub fn send_request(&mut self, request: &Request) -> Result<Response> {
-        serde_json::to_writer(&mut self.writer, &request)?;
-        self.writer.flush()?;
+        serde_json::to_writer(&mut self.tcp_stream, &request)?;
         Ok(Response::deserialize(&mut Deserializer::from_reader(
-            &mut self.reader,
+            &self.tcp_stream,
         ))?)
     }
 }
