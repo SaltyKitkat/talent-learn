3fc3e926106be26fc267ac0a5dce6ee764aad55a
SaltyKitkat <954537646@qq.com>
2022-03-22 12:35:36 +0800

    Revert "not use bufio in client"
    This reverts commit 459680e2e8a81794858339a61bd75a7a9bff6183.

--- src/client.rs
+++ src/client.rs
@@ -4,21 +4,28 @@ use crate::{
 };
 use serde::Deserialize;
 use serde_json::Deserializer;
-use std::net::{TcpStream, ToSocketAddrs};
+use std::{
+    io::{BufReader, BufWriter, Write},
+    net::{TcpStream, ToSocketAddrs},
+};
 
 pub struct KvsClient {
-    tcp_stream: TcpStream,
+    reader: BufReader<TcpStream>,
+    writer: BufWriter<TcpStream>,
 }
 
 impl KvsClient {
     pub fn connect(socket: impl ToSocketAddrs) -> Result<Self> {
-        let tcp_stream = TcpStream::connect(socket)?;
-        Ok(Self { tcp_stream })
+        let stream = TcpStream::connect(socket)?;
+        let reader = BufReader::new(stream.try_clone()?);
+        let writer = BufWriter::new(stream);
+        Ok(Self { reader, writer })
     }
     pub fn send_request(&mut self, request: &Request) -> Result<Response> {
-        serde_json::to_writer(&mut self.tcp_stream, &request)?;
+        serde_json::to_writer(&mut self.writer, &request)?;
+        self.writer.flush()?;
         Ok(Response::deserialize(&mut Deserializer::from_reader(
-            &self.tcp_stream,
+            &mut self.reader,
         ))?)
     }
 }
