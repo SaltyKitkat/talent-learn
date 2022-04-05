28a3e1b430706bb2855fad4d483540e69ea4aaeb
SaltyKitkat <954537646@qq.com>
2022-04-05 11:24:55 +0800

    update

--- src/lib.rs
+++ src/lib.rs
@@ -3,6 +3,7 @@ pub mod client;
 pub mod engine;
 pub mod error;
 pub mod server;
+pub mod thread_pool;
 
 pub use engine::kvstore::KvStore;
 pub use engine::KvsEngine;
---
+++ src/thread_pool/mod.rs
@@ -0,0 +1,16 @@
+struct ThreadPool;
+
+impl ThreadPool {
+    fn new() -> Self {
+        todo!()
+    }
+
+    fn pushjob(&mut self, job: impl FnOnce()) {
+        todo!()
+    }
+}
+impl Drop for ThreadPool {
+    fn drop(&mut self) {
+        todo!()
+    }
+}
